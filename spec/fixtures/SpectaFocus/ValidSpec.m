6542621b495361d08d71b051a585c6a57203a1eb
Wes Smith <wes@meetup.com>
2014-04-17 22:50:02 -0400

    specs for specta focus check

---
+++ spec/fixtures/SpectaFocus/ValidSpec.m
@@ -0,0 +1,16 @@
+#import "Specta.h"
+
+SpecBegin(Focus)
+
+context(@"valid context", ^{
+  describe(@"valid describe", ^{
+    example(@"valid example" ^{
+    });
+    it(@"valid it", ^{
+    });
+    specify(@"valid specify", ^{
+    });
+  });
+});
+
+SpecEnd
